1d84633f3b85426f4cf5bc78b9fc3b4d44ebdea5
Antonio Cuadros Lapresta <antculap@gmail.com>
2020-10-17 18:58:52 +0200

    #5, understanding opencv video loading

--- src/video_utilities.py
+++ src/video_utilities.py
@@ -7,3 +7,22 @@
 
 import cv2
 import numpy as np
+
+class VideoAnalizer:
+    videoPath = None
+
+    #Constructor
+    #Gets the video path
+    def __init__(self, videoPath):
+        self.videoPath = videoPath
+    
+    #Analize the video frame per frame
+    def analyze(self):
+        video = cv2.VideoCapture('videoPath')
+
+        while(video.isOpened()):
+            frame = cap.read()
+            cv2.imshow("frame", frame)
+            
+        cap.release()
+        cv2.destroyAllWindows()
--- src/zepazo.py
+++ src/zepazo.py
@@ -6,7 +6,12 @@
 ##############################
 
 import  argparse
+from video_utilities import *
 
 parser = argparse.ArgumentParser(description="Zepazo: Get moon impacts frames")
 parser.add_argument( "-v", "--video", type=str, help="Video file path to analize")
 args = parser.parse_args()
+
+print(args.video)
+
+video_analizer = VideoAnalizer(args.video)
