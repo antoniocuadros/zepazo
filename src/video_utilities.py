d3b3c5b5d3e4889ef7ae12f3a59b3c2ab4657e09
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-02-11 18:22:13 +0100

    #7 [US1], improved video loading

--- src/video_utilities.py
+++ src/video_utilities.py
@@ -7,9 +7,14 @@
 
 import cv2
 import numpy as np
+import datetime 
+import ntpath
 
 class VideoAnalizer:
     videoPath = None
+    fps = None
+    frames = None
+    seconds = None
 
     #Constructor
     #Gets the video path
@@ -19,27 +24,35 @@ class VideoAnalizer:
 
     #Analize the video frame per frame
     def analyze(self):
-        try:
-            #Check if file exists
-            f = open(self.videoPath)
-            f.close()
+        #Play the video
+        cap = cv2.VideoCapture(self.videoPath)
+        
+        #If we dont find the file, error
+        if (cap.isOpened()== False): 
+            print("Video could not be found on this path")
 
-            #Play the video
-            cap = cv2.VideoCapture(self.videoPath)
+        #We find the file
+        else:
+
             play = True
 
             while(cap.isOpened() and play):
                 ret, frame=cap.read() #read a single frame, ret will be true if frame captured
-                cv2.imshow('output', frame)
                 
-                if(cv2.waitKey(1) & 0xFF == ord('q')):
+                #If there are frames left
+                if ret == True:
+                    cv2.imshow(ntpath.basename(self.videoPath),frame)
+                    if(cv2.waitKey(1) & 0xFF == ord('q')):
+                        play = False
+                #No more frames, exit loop
+                else:
                     play = False
-
+            
             cap.release()
             cv2.destroyAllWindows()
-        except:
-            print("Video could not be found on this path")
-            
+
+
+        
 
 
         
