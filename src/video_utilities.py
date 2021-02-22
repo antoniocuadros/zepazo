0a94039ba9bb1e5238c313babe046124ae9b9e3b
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-02-22 17:04:02 +0100

    closes 9 deleted redundant check

--- src/video_utilities.py
+++ src/video_utilities.py
@@ -38,30 +38,23 @@ class VideoAnalizer:
     def analyze(self):
         cap = self.videoCapture
         
-        #If we dont find the file, error
-        if (cap.isOpened()== False): 
-            print("Video could not be found on this path")
+        play = True
 
-        #We find the file
-        else:
-
-            play = True
-
-            while(cap.isOpened() and play):
-                ret, frame=cap.read() #read a single frame, ret will be true if frame captured
-                
-                #If there are frames left
-                if ret == True:
-                    cv2.imshow(ntpath.basename(self.videoPath),frame)
-                    
-                    if(cv2.waitKey(1) & 0xFF == ord('q')):
-                        play = False
-                #No more frames, exit loop
-                else:
-                    play = False
+        while(cap.isOpened() and play):
+            ret, frame=cap.read() #read a single frame, ret will be true if frame captured
             
-            cap.release()
-            cv2.destroyAllWindows()
+            #If there are frames left
+            if ret == True:
+                cv2.imshow(ntpath.basename(self.videoPath),frame)
+                
+                if(cv2.waitKey(1) & 0xFF == ord('q')):
+                    play = False
+            #No more frames, exit loop
+            else:
+                play = False
+        
+        cap.release()
+        cv2.destroyAllWindows()
 
     def __getGeneralVideoStats(self, cap):
             self.frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
