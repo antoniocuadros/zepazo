854fe43c61229be12f6410a1e505728e32d160eb
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-02-24 18:14:58 +0100

    #15 we can get images with a rectangle in the difference between two frames

--- src/video_utilities.py
+++ src/video_utilities.py
@@ -131,3 +131,23 @@ class VideoAnalizer:
 
         #get the difference between frame1 and frame2
         difference = cv2.subtract(grayFrame1, grayFrame2, difference)
+
+        #For each pixel, if the pixel value is smaller than a value (second argument: 50) 
+        #it is set to 0 (white), if not the pixel is set to a maximum value (third argument) (black)
+        ret, threshed_img = cv2.threshold(np.array(difference, dtype=np.uint8), 50, 255, cv2.THRESH_BINARY)
+
+        #We obtain the contours of the image
+        contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        impact_count = 0
+        #for each contour finded we draw a rectangle and we save the image
+        for c in contours:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame1, (x-10, y-10), (x+w+10, y+h+10), (0, 0, 255), 2)
+
+            cv2.imwrite(self.videoPath + "_" + str(impact_count) + ".png", frame1)
+            impact_count = impact_count + 1
+
+        
+        
+        self.showFrame(frame1)
