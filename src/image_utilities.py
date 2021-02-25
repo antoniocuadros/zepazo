356b100f2952b839e05ae74a5d03a0625cbbcd7a
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-02-25 17:04:47 +0100

    closes #16, new class added

---
+++ src/image_utilities.py
@@ -0,0 +1,17 @@
+#Python 3.8.2
+##############################
+#
+#   image_utilities.py
+#
+##############################
+
+import cv2
+import numpy as np
+
+class ImageAnalizer:
+    """
+    This class will represent an Image Analizer with multiple tools for working with images.
+    """
+    def __init__:
+        pass
+    
