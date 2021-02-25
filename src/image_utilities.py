49cc99f40c38418ff57a3748ed36a60e00a5e0a4
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-02-25 17:18:20 +0100

    closes #18 method moved and improved

--- src/image_utilities.py
+++ src/image_utilities.py
@@ -35,3 +35,44 @@ class ImageAnalizer:
         else: 
             cv2.imwrite(name + ".png", image)
             return True
+
+    def getDifferences(self, frame1, frame2):
+        """
+        Subtract the two frames to get the differences.
+        :param: frame1: video frame 1.
+        :type: frame1: numpy.ndarray.
+
+        :param: frame2: video frame 2.
+        :type: frame1: numpy.ndarray.
+
+        :return: Returns a new frame resulting from subtracting frame2 from frame1
+        :rtype: numpy.ndarray.
+        """
+        
+        #reserve memory for the resulting image
+        difference = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+
+        #from RGB space to GrayScale space for simplicity
+        grayFrame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        grayFrame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+
+        #get the difference between frame1 and frame2
+        difference = cv2.subtract(grayFrame1, grayFrame2, difference)
+
+        #For each pixel, if the pixel value is smaller than a value (second argument: 50) 
+        #it is set to 0 (white), if not the pixel is set to a maximum value (third argument) (black)
+        ret, threshed_img = cv2.threshold(np.array(difference, dtype=np.uint8), 50, 255, cv2.THRESH_BINARY)
+
+        #We obtain the contours of the image
+        contours, _ = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        impact_count = 0
+        #for each contour finded we draw a rectangle and we save the image
+        for c in contours:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame1, (x-10, y-10), (x+w+10, y+h+10), (0, 0, 255), 2)
+
+            #cv2.imwrite(self.videoPath + "_" + str(impact_count) + ".png", frame1)
+            impact_count = impact_count + 1
+
+        return frame1
--- src/video_utilities.py
+++ src/video_utilities.py
@@ -10,6 +10,8 @@ import numpy as np
 import datetime 
 import ntpath
 
+from image_utilities import *
+
 class VideoAnalizer:
     """
     This class will represent a Video Analizer with multiple tools for working with video.
@@ -33,6 +35,9 @@ class VideoAnalizer:
         if not (self.videoCapture).isOpened():
             raise Exception("Video could not be found on this path")
 
+        # -> Object to work with images
+        self.imageAnalizer = ImageAnalizer()
+
         #Gets: 
         # -> frames
         # -> fps
@@ -55,9 +60,9 @@ class VideoAnalizer:
             ret, frame2=cap.read()
             #If there are frames left
             if ret == True:    
+                frame = (self.imageAnalizer).getDifferences(frame, frame2)
                 if self.showVideo: 
-                    #self.showFrame(frame)
-                    self.getDifferences(frame, frame2)
+                    self.showFrame(frame)
 
                 if(cv2.waitKey(100) & 0xFF == ord('q')):
                     play = False
@@ -109,43 +114,3 @@ class VideoAnalizer:
         print("Duration: ", str(datetime.timedelta(seconds=self.seconds)))
         
 
-    def getDifferences(self, frame1, frame2):
-        """
-        Subtract the two frames to get the differences.
-        :param: frame1: video frame 1.
-        :type: frame1: numpy.ndarray.
-
-        :param: frame2: video frame 2.
-        :type: frame1: numpy.ndarray.
-
-        :return: Returns a new frame resulting from subtracting frame2 from frame1
-        :rtype: numpy.ndarray.
-        """
-        
-        #reserve memory for the resulting image
-        difference = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-
-        #from RGB space to GrayScale space for simplicity
-        grayFrame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-        grayFrame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
-        #get the difference between frame1 and frame2
-        difference = cv2.subtract(grayFrame1, grayFrame2, difference)
-
-        #For each pixel, if the pixel value is smaller than a value (second argument: 50) 
-        #it is set to 0 (white), if not the pixel is set to a maximum value (third argument) (black)
-        ret, threshed_img = cv2.threshold(np.array(difference, dtype=np.uint8), 50, 255, cv2.THRESH_BINARY)
-
-        #We obtain the contours of the image
-        contours, _ = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
-        impact_count = 0
-        #for each contour finded we draw a rectangle and we save the image
-        for c in contours:
-            x, y, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame1, (x-10, y-10), (x+w+10, y+h+10), (0, 0, 255), 2)
-
-            #cv2.imwrite(self.videoPath + "_" + str(impact_count) + ".png", frame1)
-            impact_count = impact_count + 1
-
-        self.showFrame(frame1)
