285d3a0e95907021a014a6cfcd51972d1731a3e9
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-02-25 17:09:52 +0100

    #17, added method to save images

--- src/image_utilities.py
+++ src/image_utilities.py
@@ -7,6 +7,7 @@
 
 import cv2
 import numpy as np
+import os.path
 
 class ImageAnalizer:
     """
@@ -15,3 +16,22 @@ class ImageAnalizer:
     def __init__:
         pass
     
+    def saveImage(image, name):
+        """
+        Save an image in the current working directory.
+        :param: image: the image to save.
+        :type: image: numpy.ndarray.
+
+        :param: name: name of the image to save.
+        :type: frame1: str.
+
+        :return: Returns true if the image is saved correctly.
+        :rtype: Boolean.
+        """
+
+        #Check if filename exists
+        if (os.path.isfile(name +'.png')):
+            return False
+        else: 
+            cv2.imwrite(name + ".png", image)
+            return True
