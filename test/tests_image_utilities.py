14490dd0313ec4a77341c839568856cd59a05440
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-03-02 13:58:26 +0100

    closes #15 added test to check differences

--- test/tests_image_utilities.py
+++ test/tests_image_utilities.py
@@ -4,16 +4,14 @@
 #   image_utilities.py
 #
 ##############################
+import pytest
 
 import cv2
 import numpy as np
 import os.path
 from .argument import Argument
-
-import pytest
 import sys
 import  argparse
-import numpy as np
 from src.Analyzers.image_utilities import ImageAnalyzer
 from src.Analyzers.video_utilities import VideoAnalyzer
 
@@ -36,3 +34,22 @@ def test_if_image_is_saved():
     image_analyzer.saveImage(frame, 'test_image')  
     assert os.path.isfile('test_image.png') == True
     os.remove("test_image.png")
+
+################################################
+#
+#
+#
+# Checks if an image is obtained as two frame difference
+#
+################################################
+
+def test_if_difference_image_is_ok():
+
+    image_analyzer = ImageAnalyzer
+    arguments = Argument('videos/test.mp4', 'True')
+    video_analyzer = VideoAnalyzer(arguments)
+    cap = video_analyzer.videoCapture
+
+    ret, frame = cap.read()
+    resulting_frame = image_analyzer.getDifferences(image_analyzer, frame, frame)  
+    assert np.max(resulting_frame[0][0]) <= 1
