cc6d910de0fcdab5a92c908d9c30f43ee59240ce
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-03-31 16:39:48 +0200

    progresses #34, now we can put several masks

--- src/zepazo.py
+++ src/zepazo.py
@@ -22,7 +22,7 @@ ARGS_DOCS = ['-v', '../videos/test.mp4']
 parser = argparse.ArgumentParser(description="Zepazo: Get moon impacts frames")
 parser.add_argument( "-v", "--video", type=str, help="Video file path to analize")
 parser.add_argument( "-s", "--show", type=bool, help="Show video during analysis")
-parser.add_argument( "-m", "--mask", type=int, help="Place num_masks rectangular masks")
+parser.add_argument( "-mm", "--mousemask", type=int, help="Place num_masks rectangular masks")
 
 
 if ( Path(argv[0]).name != 'sphinx-build' ):
@@ -38,7 +38,7 @@ video_analizer = VideoAnalyzer(args)
 
 #Pre Processing
 # -> Apply a mask
-if(args.mask != None):
+if(args.mousemask != None):
     video_analizer.selectAndApplyMask(args.mask)
 
 #Proccessing
