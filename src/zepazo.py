fda07bf3baaabff782d9b54ea7526f0415b665a7
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-04-04 18:17:19 +0200

    Progresses #37 added argument to get helpplacing circlelimit argument

--- src/zepazo.py
+++ src/zepazo.py
@@ -26,7 +26,7 @@ parser.add_argument( "-mm", "--mousemask", type=int, help="Place num_masks recta
 parser.add_argument( "-cm", "--coordinatesmask",nargs='+', type=int, help="Place num_masks rectangular masks  by giving a list of points")
 parser.add_argument( "-l", "--detectionlimit", type=int, help="Detection limit (1-255) Default 50")
 parser.add_argument( "-cl", "--circlelimit", type=int, help="Moon contour detection limit (1-255) Default ~33 calculated while analysis")
-
+parser.add_argument( "-hcl", "--helpcirclelimit", type=int, help="Interface to adjust circlelimit param")
 
 if ( Path(argv[0]).name != 'sphinx-build' ):
     args = parser.parse_args()
