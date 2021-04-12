9d7f9a11be20236579dd8b42f65eae637f793ee4
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-04-12 11:36:31 +0200

    Progresses #39, reset previous button code, not this us

--- src/Interface/interface.py
+++ src/Interface/interface.py
@@ -56,32 +56,7 @@ class ZepazoParams(QMainWindow):
         self.button_visualize_all.setObjectName("button_visualize_all")
         self.layoutSuperior.addWidget(self.button_visualize_all, 0, 0, 1, 1)
 
-        #Adds a button to reset all parameters
-        self.button_reset_all = QtWidgets.QPushButton(self.layoutWidget)
-        self.button_reset_all.setMinimumSize(QtCore.QSize(30, 30))
-        self.button_reset_all.setMaximumSize(QtCore.QSize(30, 30))
-        self.button_reset_all.setText("")
-        icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("icons/reset_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.button_reset_all.setIcon(icon1)
-        self.button_reset_all.setIconSize(QtCore.QSize(18, 18))
-        self.button_reset_all.setObjectName("button_reset_all")
-        self.layoutSuperior.addWidget(self.button_reset_all, 0, 1, 1, 1)
 
-        #Adds a button to preview a video sample
-        self.button_play = QtWidgets.QPushButton(self.layoutWidget)
-        self.button_play.setMinimumSize(QtCore.QSize(30, 30))
-        self.button_play.setMaximumSize(QtCore.QSize(30, 30))
-        self.button_play.setText("")
-        icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("play_icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.button_play.setIcon(icon2)
-        self.button_play.setIconSize(QtCore.QSize(25, 25))
-        self.button_play.setObjectName("button_play")
-        self.layoutSuperior.addWidget(self.button_play, 0, 2, 1, 1)
-        
-        #Adds superior frame to general layout
-        self.layoutVerticalTresPaneles.addWidget(self.frame_superior)
 
 def launch_UI():
     app = QApplication(sys.argv)
