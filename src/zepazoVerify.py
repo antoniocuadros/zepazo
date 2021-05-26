114ff153b284990e41cf614e23870fdf8dad8b72
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-05-26 13:15:00 +0200

    Progresses #53, added file and arguments

---
+++ src/zepazoVerify.py
@@ -0,0 +1,15 @@
+import  argparse
+from Dators.fsdator import FSDator
+
+
+######################
+#
+# Defining arguments
+#
+######################
+parser = argparse.ArgumentParser(description="ZepazoVerify: From two log files checks if there is an impact taking place in time in the same period")
+parser.add_argument( "-lgf1", "--logFile1", type=str, help="Log file from the first video")
+parser.add_argument( "-lgf2", "--logFile2", type=str, help="Log file from the second video")
+
+#Getting arguments
+args = parser.parse_args()
