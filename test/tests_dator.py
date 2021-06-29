8532d12053026ceeb0cbdfb50b290ec691a1543b
Antonio Cuadros Lapresta <antculap@gmail.com>
2021-06-29 16:25:16 +0200

    Progresses #54, added file to run Dator tests

---
+++ test/tests_dator.py
@@ -0,0 +1,13 @@
+#Python 3.8.2
+##############################
+#
+#   tests_dator.py
+#
+##############################
+import pytest
+import cv2
+import os
+import runpy
+import  argparse
+from src.Dators.dator import Dator
+
